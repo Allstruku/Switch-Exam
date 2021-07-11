55e68d8c0f8c5004dee3dac72a1b76f5352eedfd
ParkJooahn <ParkJooahn@localhost>
2021-07-11 18:36:00 +0900

    Initial Commit

---
+++ src/SwitchExam.java
@@ -0,0 +1,36 @@
+
+public class SwitchExam {
+
+	public static void main(String[] args) {
+		int value = 1;
+		char type = 'a';
+			switch(type) {
+			case 'a':
+				System.out.println("Type is a");
+				break;
+			case 'b':
+				System.out.println("Type is b");
+				break;
+			case 'c':
+				System.out.println("Type is c");
+				break;
+			default:
+				System.out.println("Type is neither a, b, or c");
+			}
+			
+//			switch(value) {
+//			case 1:
+//				System.out.println("Value is 1");
+//				break;
+//			case 2:
+//				System.out.println("Value is 1");
+//				break;
+//			case 3:
+//				System.out.println("Value is 1");
+//				break;
+//			default:
+//				System.out.println("Value is 1");
+//			}
+	}
+
+}
